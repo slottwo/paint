465d0c7c32225eeae8e0398b1a11e60d5b6af760
Pedro Galvão <41028091+slottwo@users.noreply.github.com>
2023-12-21 11:01:10 -0300

    Paint Project

--- src/main.c
+++ src/main.c
@@ -1,10 +1,6 @@
 #include <GL/glut.h>
 #include <math.h>
-
-#define SCREEN_WIDTH 1366
-#define SCREEN_HEIGH 768
-#define W 400
-#define H 400
+#include "settings.h"
 
 void onInitialization()
 {
@@ -16,20 +12,7 @@ void onInitialization()
 
 void onDisplay()
 {
-    float h = sqrt(3) / 4;
-
     glClear(GL_COLOR_BUFFER_BIT);
-
-    glBegin(GL_TRIANGLES);
-
-    glColor3f(1, 0, 0);
-    glVertex2f(0.25, (1 - h) / 2);
-    glColor3f(0, 1, 0);
-    glVertex2f(0.75, (1 - h) / 2);
-    glColor3f(0, 0, 1);
-    glVertex2f(0.5, (1 + h) / 2);
-
-    glEnd();
 
     glFlush();
 }
@@ -44,16 +27,16 @@ int main(int argc, char const *argv[])
         (SCREEN_WIDTH - W) / 2,
         (SCREEN_HEIGH - H) / 2);
     glutInitWindowSize(W, H);
-    glutCreateWindow("Triangle");
+    glutCreateWindow("Paint");
 
     // Drawing presets
     onInitialization();
 
     // Draw
     glutDisplayFunc(onDisplay);
+
     // Loop
     glutMainLoop();
 
-    // End
     return 0;
 }
