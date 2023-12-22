34f98ede1751f21082d10853b2cabb22bce44590
Pedro Galvão <41028091+slottwo@users.noreply.github.com>
2023-12-22 11:14:19 -0300

    Fix imports

--- run.sh
+++ run.sh
@@ -2,9 +2,10 @@
 
 for lib in lib/*.c; do
     if [ -f "$lib" ]; then
-        gcc -c "$lib" -o "${lib%.c}.o"
+        gcc -c "$lib" -o "${lib%.c}.o" -lm -lGL -lglut -lGLU
     fi
 done
+
 
 file=$(basename "$1")
 
@@ -12,9 +13,12 @@ if [ ! -d "bin/${1%$file}" ]; then
     mkdir -p "bin/${1%$file}"
 fi
 
-gcc $1 -o "bin/${1%.c}" -lm -lGL -lglut -lGLU
+gcc $1 -c -lm -lGL -lglut -lGLU
+
+gcc lib/*.o *.o -o "bin/${1%.c}" -lm -lGL -lglut -lGLU
 
 rm lib/*.o
+rm *.o
 
 if [ $# -gt 1 ]; then
     "./bin/${1%.c}" "${@:2}"
