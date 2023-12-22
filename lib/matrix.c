6aa56e26bd2905f591e3d67d8cc97cfcb46d95f5
Pedro Galvão <41028091+slottwo@users.noreply.github.com>
2023-12-22 11:15:30 -0300

    Optimization and transformation methods

--- lib/matrix.c
+++ lib/matrix.c
@@ -1,29 +1,52 @@
-#include "stdlib.h"
+#include <stdlib.h>
 #include "matrix.h"
-#include "objects.h"
+#include <math.h>
 
 /**
- * @brief Multiplies 3*3 square matrices (R = A * B)
+ * @brief Multiply two 3*3 square matrices
  *
  * @param A First double[9] vector
  * @param B Second double[9] vector
- * @param R Resulting double[9] vector
+ * @return double* – Resulting vector
  */
-void multiply(double *A, double *B, double *R)
+double *generic_multiply(double *A, double *B)
 {
     if (A == NULL | B == NULL)
         exit(1);
 
-    if (R == NULL)
-        R = (double *)calloc(9, sizeof(double));
-    else
-        for (int i = 0; i < 9; i++)
-            R[i] = 0;
+    double *R = (double *)calloc(9, sizeof(double));
 
     for (int i = 0; i < 3; i++)
         for (int j = 0; j < 3; j++)
             for (int k = 0; k < 3; k++)
                 R[3 * i + j] += A[3 * i + k] * B[3 * k + j];
+
+    return R;
+}
+
+// a b c   g h i   ag + bj | ah + bk | ai + bl
+// d e f x j k l = dg + ej | dh + bk | di + el
+// 0 0 1   0 0 1      0    |    0    |    1
+
+/**
+ * @brief Multiply transformation matrices optimally
+ *
+ * @param A First double[9] vector
+ * @param B Second double[9] vector
+ * @return double* – Resulting vector
+ */
+double *optimized_multiply(double *A, double *B)
+{
+    double *R = (double *)calloc(9, sizeof(double));
+
+    R[0] = A[0] * B[0] + A[1] * B[3];
+    R[1] = A[0] * B[1] + A[1] * B[4];
+    R[2] = A[0] * B[2] + A[1] * B[5];
+    R[3] = A[3] * B[0] + A[4] * B[3];
+    R[4] = A[3] * B[1] + A[4] * B[4];
+    R[5] = A[3] * B[2] + A[4] * B[5];
+
+    return R;
 }
 
 /**
@@ -39,4 +62,33 @@ void transform(Point *P, double *T)
 
     *P[0] = *P[0] * T[0] + *P[1] * T[1] + T[2];
     *P[0] = *P[0] * T[3] + *P[1] * T[4] + T[5];
+}
+
+double *get_rotate_matrix(double angle)
+{
+    double *R = (double *)calloc(9, sizeof(double));
+    R[0] = cos(angle);
+    R[1] = sin(angle);
+    R[3] = -sin(angle);
+    R[4] = cos(angle);
+    R[8] = 1;
+    return R;
+}
+
+double *get_scale_matrix(double x, double y)
+{
+    double *R = (double *)calloc(9, sizeof(double));
+    R[0] = x;
+    R[4] = y;
+    R[8] = 1;
+    return R;
+}
+
+double *get_translate_matrix(double x, double y)
+{
+    double *R = (double *)calloc(9, sizeof(double));
+    R[2] = x;
+    R[5] = y;
+    R[8] = 1;
+    return R;
 }
--- lib/matrix.h
+++ lib/matrix.h
@@ -1,1 +1,8 @@
-void multiply(double *, double *, double *);
+#include "objects.h"
+
+void generic_multiply(double *, double *, double *);
+void optimized_multiply(double *, double *, double *);
+void transform(Point *, double *);
+double *get_rotate_matrix(double);
+double *get_scale_matrix(double, double);
+double *get_translate_matrix(double, double);
