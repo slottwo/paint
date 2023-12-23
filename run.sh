366837deff04e6323dbae03f681d8a65f48c236c
Pedro Galvão <41028091+slottwo@users.noreply.github.com>
2023-12-23 18:45:59 -0300

    Remove unused path to lib/*.o

--- run.sh
+++ run.sh
@@ -4,10 +4,14 @@ if [ ! -d "bin" ]; then
     mkdir -p "bin"
 fi
 
+# Compile custom libs
+
 find lib/ -type f -name '*.c' | while read -r file; do
     output_file="bin/$(basename "${file%.c}.o")"
     gcc -c "$file" -o "$output_file" -lm -lGL -lglut -lGLU
 done
+
+# Compile main file
 
 file=$(basename "$1")
 
@@ -17,10 +21,15 @@ fi
 
 gcc -c $1 -o "bin/${file%.c}.o" -lm -lGL -lglut -lGLU
 
-gcc lib/*.o bin/*.o -o "bin/${1%.c}" -lm -lGL -lglut -lGLU
+# Link everyone
 
-rm bin/*.o
-# find . -type f -name "*.o" -delete
+gcc bin/*.o -o "bin/${1%.c}" -lm -lGL -lglut -lGLU
+
+# Remove object files
+
+rm bin/*.o # find . -type f -name "*.o" -delete
+
+# Run
 
 if [ $# -gt 1 ]; then
     "./bin/${1%.c}" "${@:2}"
