34eda1eeb29f139320010a9eeefc5ca5df5f2aed
Pedro Galvão <41028091+slottwo@users.noreply.github.com>
2023-12-23 18:46:46 -0300

    Test getVertex

--- test/objects.c
+++ test/objects.c
@@ -1,7 +1,11 @@
-#include "../lib/objects/polygon.h"
+#include <stdio.h>
+#include "../lib/render.h"
 
 int main(int argc, char const *argv[])
 {
     Polygon *p = createPolygon();
+    polygonPush(p, createPointXY(1.0,2.0));
+    double *v = getVertex(p->head);
+    printf("%.1f %.1f\n", v[0], v[1]);
     return 0;
 }
