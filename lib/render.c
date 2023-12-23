6640fe7f5279ce44c5e5e8a83911282e7f582f97
Pedro Galvão <41028091+slottwo@users.noreply.github.com>
2023-12-23 16:07:56 -0300

    render methods sketch-up

---
+++ lib/render.c
@@ -0,0 +1,54 @@
+#include <GL/freeglut.h>
+#include "render.h"
+
+int renderPoint(Point *point)
+{
+    // if (point == NULL) { ... }
+
+    glBegin(GL_POINT);
+    glVertex2d(point->x, point->y);
+    glEnd();
+}
+
+// int renderPoints(Point **points)
+// {
+//     /* code */
+// }
+
+int renderPoints(Polygon *polygon)
+{
+    glBegin(GL_POINTS);
+    // ...
+    glEnd();
+}
+
+// int renderLine(Line *);
+
+int renderLines(Polygon *polygon)
+{
+    glBegin(GL_LINE_STRIP);
+
+    Node *node = polygon->head;
+    while (node->next != NULL)
+    {
+        glVertex2dv(getVertex(node));
+        node = node->next;
+    }
+
+    glEnd();
+}
+
+int renderPolygon(Polygon *polygon)
+{
+    glBegin(GL_POLYGON);
+
+    Node *node = polygon->head;
+    while (node != NULL)
+    {
+        glVertex2dv(getVertex(node));
+        node = node->next;
+    }
+    glVertex2dv(getVertex(polygon->head));
+
+    glEnd();
+}
--- lib/render.h
+++ lib/render.h
@@ -1,6 +1,8 @@
-#include "lib/objects.h"
+#include "objects/point.h"
+#include "objects/line.h"
+#include "objects/polygon.h"
 
 int renderPoint(Point *);
-int renderLine(Line *);
+// int renderLine(Line *);
+int renderLines(Polygon *);
 int renderPolygon(Polygon *);
-int renderLineStrip(Polygon *);
