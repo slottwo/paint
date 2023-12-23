4365bc2513872165315eba24614ccf122c876fa2
Pedro Galvão <41028091+slottwo@users.noreply.github.com>
2023-12-23 15:33:35 -0300

    Uncolored point (for now)

--- lib/objects/point.c
+++ lib/objects/point.c
@@ -1,12 +1,20 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "point.h"
 
-Point *createPoint(double *coords, double *colors)
+Point *createPoint(double *coords) // , double *colors)
 {
+    if (coords == NULL)
+    {
+        printf("Point Creation Error: NULL coords received\n");
+        exit(1);
+        return NULL;
+    }
+
     Point *point = (double *)malloc(sizeof(Point));
     if (point == NULL)
     {
-        // printf("Point creation error: malloc fail\n");
+        printf("Point creation error: malloc fail\n");
         exit(1);
         return NULL;
     }
@@ -14,20 +22,36 @@ Point *createPoint(double *coords, double *colors)
     point->x = coords[0];
     point->y = coords[1];
 
-    if (colors == NULL)
+    // if (colors == NULL)
+    // {
+    //     point->is_colored = 0;
+    //     point->colors[0] = 0;
+    //     point->colors[1] = 0;
+    //     point->colors[2] = 0;
+    // }
+    // else
+    // {
+    //     point->is_colored = 1;
+    //     point->colors[0] = colors[0];
+    //     point->colors[1] = colors[1];
+    //     point->colors[2] = colors[2];
+    // }
+
+    return point;
+}
+
+Point *createPointv(double x, double y) // , double *);
+{
+    Point *point = (double *)malloc(sizeof(Point));
+    if (point == NULL)
     {
-        point->is_colored = 0;
-        point->colors[0] = 0;
-        point->colors[1] = 0;
-        point->colors[2] = 0;
+        printf("Point creation error: malloc fail\n");
+        exit(1);
+        return NULL;
     }
-    else
-    {
-        point->is_colored = 1;
-        point->colors[0] = colors[0];
-        point->colors[1] = colors[1];
-        point->colors[2] = colors[2];
-    }
+
+    point->x = x;
+    point->y = y;
 
     return point;
 }
@@ -36,11 +60,11 @@ void freePoint(Point *point)
 {
     if (point == NULL)
     {
-        // printf("Point deallocate error: NULL pointer received\n");
+        printf("Point deallocate error: NULL pointer received\n");
         exit(1);
     }
 
-    free(point->colors);
+    // free(point->colors);
     free(point);
 
     point = NULL;
--- lib/objects/point.h
+++ lib/objects/point.h
@@ -2,9 +2,10 @@ typedef struct point
 {
     double x;
     double y;
-    int is_colored;
-    double colors[3];
+    // int is_colored;
+    // double colors[3];
 } Point;
 
-Point *createPoint(double *, double *);
+Point *createPoint(double *);        // , double *);
+Point *createPointv(double, double); // , double *);
 void freePoint(Point *);
