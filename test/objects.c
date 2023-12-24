3ca1ec978b50cda53cf9a7edf96818f2a5cce78d
Pedro Galvão <41028091+slottwo@users.noreply.github.com>
2023-12-24 10:07:23 -0300

    More object tests

--- test/objects.c
+++ test/objects.c
@@ -3,9 +3,37 @@
 
 int main(int argc, char const *argv[])
 {
-    Polygon *p = createPolygon();
-    polygonPush(p, createPointXY(1.0,2.0));
-    double *v = getVertex(p->head);
-    printf("%.1f %.1f\n", v[0], v[1]);
+    Polygon *poly = createPolygon();
+
+    polygonPush(poly, createPointXY(1.0, 2.0));
+    polygonPush(poly, createPointXY(2.0, 2.0));
+    polygonPush(poly, createPointXY(1.0, 3.0));
+
+    double *v = getV(poly->head->vertex);
+
+    printf("- head: %.1f %.1f\n", v[0], v[1]);
+
+    printf("- len: %d\n", polygonLength(poly));
+
+    printf("- 1st 5 points:\n");
+
+    Point *point;
+    for (int i = 0; i < 5; i++)
+    {
+        point = polygonPop(poly);
+        if (point)
+        {
+            v = getV(point);
+            printf("\t%.1f %.1f\n", v[0], v[1]);
+            freePoint(point);
+        }
+        else
+        {
+            printf("\tNo more points...\n");
+        }
+    }
+
+
+
     return 0;
 }
