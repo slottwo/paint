540780a4fa63566e03b455851cad5504690a7c33
Pedro Galvão <41028091+slottwo@users.noreply.github.com>
2023-12-27 11:00:57 -0300

    Simplify run.sh

--- run.sh
+++ run.sh
@@ -1,32 +1,26 @@
 #!/bin/bash
 
-if [ ! -d "bin" ]; then
-    mkdir -p "bin"
-fi
-
-if [ ! -d "obj" ]; then
-    mkdir -p "obj"
-fi
+mkdir -p bin obj
 
 # Compile C files
 
-find src/ -type f -name '*.c' | while read -r file; do
-    gcc -c "$file" -o "obj/$(basename "${file%.c}.o")" -lm -lGL -lglut -lGLU
+find src/ -type f -name '*.c' | while read -r f; do
+    output='obj/$(basename ${f%.c}.o)'
+    gcc -c '$f' -o '$output' -lm -lGL -lGLU -lglut
 done
 
-# Link objects
+# Link object files
 
-gcc obj/*.o -o "bin/paint.exe" -lm -lGL -lglut -lGLU
+gcc obj/*.o -o 'bin/paint' -lm -lGL -lGLU -lglut
 
-# Remove object files (?)
+# Remove object files
 
-# rm bin/*.o 
-# find . -type f -name "*.o" -delete
+rm obj/*.o
 
 # Run
 
 if [ $# -gt 1 ]; then
-    "./bin/paint.exe" "${@:1}"
+    './bin/paint' '${@:1}'
 else
-    "./bin/paint.exe"
+    './bin/paint'
 fi
