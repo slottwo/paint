13516a5b9b7030c842e248fc782bf56809092a63
Pedro Galvão <41028091+slottwo@users.noreply.github.com>
2024-01-07 09:44:16 -0300

    Fix single quote to double on run.sh

--- run.sh
+++ run.sh
@@ -4,23 +4,24 @@ mkdir -p bin obj
 
 # Compile C files
 
-find src/ -type f -name '*.c' | while read -r f; do
-    output='obj/$(basename ${f%.c}.o)'
-    gcc -c '$f' -o '$output' -lm -lGL -lGLU -lglut
+find src/ -type f -name "*.c" | while read -r f; do
+    echo "$f"
+    output="obj/$(basename ${f%.c}.o)"
+    gcc -c "$f" -o "$output" -lm -lGL -lGLU -lglut
 done
 
 # Link object files
 
-gcc obj/*.o -o 'bin/paint' -lm -lGL -lGLU -lglut
+gcc "obj/*.o" -o "bin/paint" -lm -lGL -lGLU -lglut
 
 # Remove object files
 
-rm obj/*.o
+rm "obj/*.o"
 
 # Run
 
 if [ $# -gt 1 ]; then
-    './bin/paint' '${@:1}'
+    "./bin/paint" "${@:1}"
 else
-    './bin/paint'
+    "./bin/paint"
 fi
