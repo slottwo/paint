0022dcb3657bcd2e0374e1c9479efaf2f9415955
Pedro Galvão <41028091+slottwo@users.noreply.github.com>
2024-01-07 10:30:06 -0300

    Fix "obj/*.o"

--- run.sh
+++ run.sh
@@ -5,18 +5,19 @@ mkdir -p bin obj
 # Compile C files
 
 find src/ -type f -name "*.c" | while read -r f; do
-    echo "$f"
+    echo "Compiling $f..."
     output="obj/$(basename ${f%.c}.o)"
     gcc -c "$f" -o "$output" -lm -lGL -lGLU -lglut
+    
 done
 
 # Link object files
 
-gcc "obj/*.o" -o "bin/paint" -lm -lGL -lGLU -lglut
+gcc obj/*.o -o bin/paint -lm -lGL -lGLU -lglut
 
 # Remove object files
 
-rm "obj/*.o"
+rm obj/*.o
 
 # Run
 
